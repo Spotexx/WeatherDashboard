eef359d758ece77131c90ddd0e7f70be8bf3c2a2
Spotexx <travesimo@gmail.com>
2022-01-28 19:14:35 -0500

    set up local storage for cities

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,29 @@
 //api search https://www.loc.gov/
 let searchResultsWeather = {};
 let searchResultsForecast = {};
+let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+
 $('form').submit(function(e){
     e.preventDefault();
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+$('#city').val() +','+ $('option:selected', this).attr('id') +'&appid=0c8597f899f97d65766478ecb9ae2427')
     .then(response => response.json())
     .then(data => {
         searchResultsWeather = data;
-        console.log(searchResultsWeather);
+        //adds city to savedCities local storage
+        if(savedCities.indexOf(searchResultsWeather.name) === -1){
+            console.log('city not found')
+            savedCities.push(searchResultsWeather.name);
+            localStorage.setItem("savedCities", JSON.stringify(savedCities));
+        }
     });
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+$('#city').val() +','+ $('option:selected', this).attr('id') +'&appid=0c8597f899f97d65766478ecb9ae2427')
     .then(response => response.json())
     .then(data => {
         searchResultsForecast = data;
-        console.log(searchResultsForecast);
     });
 });
+
+
 
 //current and future
 // section1 => city name + date, temp, wind, humidity, windspeed, colored uv index
