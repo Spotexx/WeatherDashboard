60bb238f56e3791a82157c673370d7b87fe6b6f2
Spotexx <travesimo@gmail.com>
2022-01-29 12:59:20 -0500

    added clause will not record city if api returns 404

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,47 @@
 //api search https://www.loc.gov/
 let searchResultsWeather = {};
 let searchResultsForecast = {};
-let savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+let savedCities = JSON.parse(localStorage.getItem("savedCities")) || {};
+let savedCountries = JSON.parse(localStorage.getItem("savedCountries")) || {};
 
-$('form').submit(function(e){
+//adds saved cities to dashboard
+let loadSavedCities = () => {
+    Object.keys(savedCities).forEach(city => {
+        $('#saved-cities').append(` <button class="btn btn-secondary w-100 mt-1" id="${city},${savedCities[city]}" onclick="getForecast(this.id)">
+                                        ${city.charAt(0).toUpperCase() + city.slice(1)}
+                                    </button>`
+                                    );
+    });
+}
+
+
+$('form').submit(function (e) {
     e.preventDefault();
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+$('#city').val() +','+ $('option:selected', this).attr('id') +'&appid=0c8597f899f97d65766478ecb9ae2427')
-    .then(response => response.json())
-    .then(data => {
-        searchResultsWeather = data;
-        //adds city to savedCities local storage
-        if(savedCities.indexOf(searchResultsWeather.name) === -1){
-            console.log('city not found')
-            savedCities.push(searchResultsWeather.name);
-            localStorage.setItem("savedCities", JSON.stringify(savedCities));
-        }
-    });
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+$('#city').val() +','+ $('option:selected', this).attr('id') +'&appid=0c8597f899f97d65766478ecb9ae2427')
-    .then(response => response.json())
-    .then(data => {
-        searchResultsForecast = data;
-    });
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + $('#city').val() + ',' + $('option:selected', this).attr('id') + '&appid=0c8597f899f97d65766478ecb9ae2427')
+        .then(response => response.json())
+        .then(data => {
+            searchResultsWeather = data;
+            console.log(searchResultsWeather);
+            //adds city to savedCities local storage
+            if (!savedCities.hasOwnProperty($('#city').val().toLowerCase()) && searchResultsWeather.cod !== "404") {
+                //sets key as city name and value as country code
+                savedCities[$('#city').val()] = $('option:selected', this).attr('id');
+                localStorage.setItem("savedCities", JSON.stringify(savedCities));
+                //adds city button to id saved-cities
+                $('#saved-cities').append(`<button class="btn btn-secondary w-100 mt-1" id="${searchResultsWeather.name}" onclick="getForecast(this.id)">${searchResultsWeather.name}</button>`);
+            }
+        });
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + $('#city').val() + ',' + $('option:selected', this).attr('id') + '&appid=0c8597f899f97d65766478ecb9ae2427')
+        .then(response => response.json())
+        .then(data => {
+            searchResultsForecast = data;
+        });
 });
 
+let init = () => {
+    loadSavedCities();
+}
+init();
 
 
 //current and future
